6d3dc7730a9b49aea95ab64116488e52c71f8005
Thomas Gelf <thomas@gelf.net>
2021-10-30 11:43:21 +0200

    Initial import

---
+++ tests/BufferedLineReaderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace gipfl\Stream;
+
+use gipfl\Test\AsyncTestUtils;
+use PHPUnit\Framework\TestCase;
+
+class BufferedLineReaderTest extends TestCase
+{
+    use AsyncTestUtils;
+
+    public function testSimple()
+    {
+        $reader = new BufferedLineReader("\n", $this->loop());
+        $lines = [];
+        $reader->on('line', function ($line) use (&$lines) {
+            $lines[] = $line;
+        });
+        $this->loop->futureTick(function () use ($reader, & $lines) {
+            $reader->write("a\nb\nc\n");
+            $this->loop()->addTimer(1, function () use (& $lines) {
+                $this->assertEquals(['a', 'b', 'c'], $lines);
+            });
+        });
+        $this->loop()->run();
+    }
+}
